0d3b4106029356c3f8c4449fdbc147bc53316846
sparrow <1457756200@qq.com>
2023-07-19 23:51:21 +0800

    新增加了测试文件，用来测试分支

---
+++ test01.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * @brief 
+ * 
+ * @param a 
+ * @param b 
+ * @return int 
+ */
+int main(void *a, void *b)
+{
+    printf("hello world\n");
+    printf("你好世界！\n");
+    system("pause");
+
+    return 0;
+}
